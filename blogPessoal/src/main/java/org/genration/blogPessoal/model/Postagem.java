7d846b25e9e028a7fb586428a0026ec431fbab46
DaniloDaSilv <danilodasilvadisanto@gmail.com>
2021-09-28 17:02:28 -0300

    Blog pessoal

--- blogPessoal/src/main/java/org/genration/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/genration/blogPessoal/model/Postagem.java
@@ -6,11 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 
 
@@ -32,6 +36,10 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -65,5 +73,14 @@ public class Postagem {
 		this.date = datw;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	
 	
 }
---
+++ blogPessoal/src/main/java/org/genration/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,12 @@
+package org.genration.blogPessoal.repository;
+
+import java.util.List;
+
+import org.genration.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
